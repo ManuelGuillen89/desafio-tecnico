73d1fc83dfb190d78211dacc9dfd6fbe18e7e597
Manuel Guillén <39125887+ManuelGuillen89@users.noreply.github.com>
2022-02-10 11:09:04 -0300

    First commit
    master_rating en aun en construcción

---
+++ master_rating.py
@@ -0,0 +1,96 @@
+from pyspark.sql import SparkSession
+from process_csv_outputs_functions import load_csv_to_spark_tempview
+import os
+
+CSV_FILE_NAMES = ['homologacion_pais', 'homologacion_rating','rating_empresa','rating_soberano']
+INPUT_DIRECTORY = "data"
+
+def load_csv_to_spark_tempview(csv_file_name, directory, spark_session):
+    dataframe = spark_session.read \
+        .option("delimiter", "|") \
+        .option("header", "true") \
+        .csv(f"{directory}/{csv_file_name}.csv")
+    dataframe.createOrReplaceTempView(csv_file_name)   
+    pass
+
+print (" >> INICIANDO GENERACION DE 'Maestro de Ratings' << ")
+
+print (" --> Cargando Spark ... ")
+spark_session = SparkSession \
+    .builder \
+    .appName("Desafío Técnico") \
+    .getOrCreate()
+
+if not (os.path.isdir(INPUT_DIRECTORY)):
+    print (f"Directorio de entrada no encontrado: INPUT_DIRECTORY = ./{INPUT_DIRECTORY}")
+    print ("Proceso abortado. Saliendo... ")
+    exit()
+
+print (" --> Cargando datasets desde archivos CSV a TempViews en Spark ... ")
+for dataset in CSV_FILE_NAMES:
+    print (f'{dataset} ...')
+    if not (os.path.isfile(f"{INPUT_DIRECTORY}/{dataset}.csv")):
+        print (f"Arhivo de entrada no econtrado: {INPUT_DIRECTORY}/{dataset}.csv")
+        print ("Proceso abortado. Saliendo... ")
+        exit()
+    load_csv_to_spark_tempview(dataset, INPUT_DIRECTORY, spark_session)
+    print (f"Dataset '{dataset}' cargado!")
+
+print (" --> Generando query ... ")
+
+#Filtra homologacion_rating dejando las agencias requeridas
+homologacion_rating_filtrado = spark_session.sql(''' 
+SELECT hr.*
+FROM homologacion_rating hr
+WHERE hr.agencia_homol IN ('SP','MDY','FITCH')
+ORDER BY hr.orden_norma DESC
+''')
+homologacion_rating_filtrado.createOrReplaceTempView("homol_rating_filt")
+homologacion_rating_filtrado.show()
+
+test_df = spark_session.sql('''
+SELECT 
+    rat_emp.rut,
+    (SELECT FIRST(hr.rating_norma) 
+        FROM homol_rating_filt hr
+        WHERE hr.rating IN (rat_no_homol.sp, rat_no_homol.mdy, rat_no_homol.fitch)
+    ) as rating
+FROM rating_empresa rat_emp,
+(SELECT sp, mdy, fitch FROM rating_empresa WHERE rating_empresa.rut = rat_emp.rut ) as rat_no_homol  
+''')
+test_df.show()
+
+""" maestro_de_ratings_sqldf = spark_session.sql('''
+SELECT 
+    rat_emp.rut, 
+    rat_emp.dv, 
+    rat_emp.nombre, 
+    homol_pais.pais,
+    (
+        SELECT FIRST(hr.rating_norma) 
+        FROM homologacion_rating hr
+        WHERE hr.agencia_homol IN ('SP','MDY','FITCH')
+        AND hr.rating IN (SELECT sp, mdy, fitch FROM rating_empresa WHERE rating_empresa.rut = rat_emp.rut )
+        ORDER BY hr.orden_norma desc
+    ) AS rating__empresa 
+FROM rating_empresa rat_emp
+LEFT JOIN homologacion_pais homol_pais ON ucase(rat_emp.pais_bbg) = ucase(homol_pais.pais_bbg)
+''')
+maestro_de_ratings_sqldf.show()   """
+
+
+""" 
+ratings_sqldf_test = spark_session.sql('''
+SELECT ra.* 
+FROM (
+    SELECT hr.* 
+    FROM homologacion_rating hr
+    WHERE hr.agencia_homol IN ('SP','MDY','FITCH')
+    order by hr.orden_norma desc
+) ra
+WHERE ra.rating IN ('Baa2','BBB+','A-')
+LIMIT 1
+''')
+ratings_sqldf_test.show() """
+
+print (" >> OPERACION FINALIZADA << ")
---
+++ process.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+
+INPUT_FILE = "data.xlsx"
+INPUT_SHEET_NAMES = ['homologacion_pais', 'homologacion_rating','rating_empresa','rating_soberano']
+OUTPUT_DIR = "data"
+
+def sheet_to_csv_from_xlsx(sheet_name, file_name, output_dir):
+    dataframe = pd.read_excel (io=file_name, sheet_name=sheet_name)
+    dataframe.to_csv (f"{output_dir}/{sheet_name}.csv", 
+                  index = None,
+                  header=True,
+                  sep='|')
+def exist_sheet_in_file(sheet_name, file_name):
+    dataframe = None
+    try:
+        dataframe = pd.read_excel (io=file_name, sheet_name=sheet_name)
+    except Exception as e:
+        print(e)
+    return dataframe.__class__ != None 
+
+print (" >> INICIANDO CONVERSION DE XLSX SHEETS EN ARCHIVOS CSV << ")
+
+if not (os.path.isfile(INPUT_FILE)):
+    print (f"Arhivo de entrada no econtrado: INPUT_FILE = ./{INPUT_FILE}")
+    print ("Proceso abortado. Saliendo... ")
+    exit()
+
+if not (os.path.isdir(OUTPUT_DIR)):
+    print (f"Directorio de salida no encontrado: OUTPUT_DIR = ./{OUTPUT_DIR}")
+    print ("Proceso abortado. Saliendo... ")
+    exit()
+
+for sheet in INPUT_SHEET_NAMES:
+    print (f'{sheet} ...')
+    if not exist_sheet_in_file(sheet_name=sheet, file_name=INPUT_FILE):
+        print (f"Hoja '{sheet}' no encontrada en el archivo '{INPUT_FILE}'")
+        print ("Proceso incompleto. Saliendo... ")
+        exit()
+    sheet_to_csv_from_xlsx(sheet, INPUT_FILE, OUTPUT_DIR)
+    print (f'{sheet} convertida y guardada!')
+
+print (" >> OPERACION FINALIZADA << ")
